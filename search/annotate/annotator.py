b015848cf600b215509b18510ded1ad95fd9d567
Steven Cox <scox@Stevens-Air.home>
2020-03-12 17:14:34 -0400

    integrated monarch api scigraph annotator

--- search/annotate/annotator.py
+++ search/annotate/annotator.py
@@ -1,27 +1,28 @@
-import requests
 import json
 import Levenshtein
+import logging
+import requests
 import sys
 import traceback
+import urllib
 import xml.etree.ElementTree as ET
+
+logger = logging.getLogger (__name__)
 
 class TOPMedStudyAnnotator:
     """ Annotate TOPMed study data with semantic knowledge graph linkages. """
     
     def __init__(self,
-                 node_normalization="https://nodenormalization-sri.renci.org/get_normalized_nodes?curie=",
-                 skip = [],
-                 ontologies = []):
-        self.node_normalization = node_normalization 
+                 config,
+                 skip = []):
+        self.normalizer = config['normalizer']
+        self.annotator = config['annotator']
         self.skip = skip
-        self.ontologies = []
-        for ontology_source in ontologies:
-            with open (ontology_source, "r") as stream:
-                self.ontologies.append (json.load(stream))
+        self.cache = {}
         
     def annotate (self, input_file):
         """
-        Tag english prose descriptions with ontology identifiers. 
+        Tag prose descriptions with ontology identifiers. 
         This operates on a dbGaP XML formatted study with a data_table root element.
         For each variable
           for each configured ontology
@@ -52,59 +53,49 @@ class TOPMedStudyAnnotator:
                 "identifiers" : {}
             }
             response.append (result)
+            try:
+                """ Annotate ontology terms in the text. """
+                encoded = urllib.parse.quote (description)
+                url = f"{self.annotator}{encoded}"
+                annotations = self.cache[url] if url in self.cache else requests.get(url).json ()
+                self.cache[url] = annotations
+                
+                """ Normalize each ontology identifier in the annotation. """
+                for span in annotations.get('spans',[]):
+                    for token in span.get('token',[]):
+                        normalized = {}
+                        curie = token.get('id', None)
+                        if not curie:
+                            continue
+                        
+                        """ Normalize the identifier with respect to the BioLink Model. """
+                        url = f"{self.normalizer}{curie}"
+                        try:
+                            normalized = self.cache[url] if url in self.cache else requests.get(url).json ()
+                            self.cache[url] = normalized
+                        except json.decoder.JSONDecodeError as e:
+                            print (f"error normalizing curie: {curie}")
 
-            similar_enough = int(len(description) * 0.3)
-            for ontology in self.ontologies:
-                for graph in ontology['graphs']:
-                    for node in graph['nodes']:
-                        id = node['id']
-                        """ Get synonyms and label """
-                        syns = map(lambda v : v['val'], node.get('meta',{}).get('synonyms',[]))
-                        label = node.get('label', None)
-                        if label:
-                            syns.append (label) 
-                        if isinstance (syns, str):
-                            syns = [ syns ]
-                        """ Iterate over all names for this thing. """
-                        for syn in syns:
-                            match = False
-                            """ There seem to be some spurious synonyms ... TODO """
-                            if syn in self.skip or len(syn) <= 3:
-                                continue
-                            if syn.isupper ():
-                                """ Consider direct matches for acronyms """
-                                match = syn == description
-                            else:
-                                """ This is useless - replace with great NLP """
-                                match = Levenshtein.distance (syn, description) < similar_enough
-                            if not match:
-                                """ Look for the presence of the synonym in the text """
-                                match = f" {syn} " in description
-                            if match:
-                                """ Ok, this variable seems to be related to this ontology entity. """
-                                normalized = {}
-                                curie = id
-                                try:
-                                    """ Normalize the identifier with respect to the BioLink Model. """
-                                    curie = id.split('/')[-1].replace('_',':')
-                                    url = f"{self.node_normalization}{curie}"
-                                    normalized = requests.get (url).json ()
-                                except:
-                                    traceback.print_exc ()
-                                preferred_id = normalized.get(curie, {}).get ("id", {})
-                                equivalent_identifiers = normalized.get(curie, {}).get ("equivalent_identifiers", [])
-                                equivalent_identifiers = [ v['identifier'] for v in equivalent_identifiers ]
-                                biolink_type = normalized.get(curie, {}).get ("type", [])
-                                if 'identifier' in preferred_id:
-                                    result['identifiers'][preferred_id['identifier']] = {
-                                        "label" : preferred_id.get('label',''),
-                                        "synonym" : syn,
-                                        "equivalent_identifiers" : equivalent_identifiers,
-                                        "type" : biolink_type
-                                    }
-                                else:
-                                    print (f"ERROR: curie:{curie} returned preferred id: {preferred_id}")
+                        """ Register results. """
+                        preferred_id = normalized.get(curie, {}).get ("id", {})
+                        equivalent_identifiers = normalized.get(curie, {}).get ("equivalent_identifiers", [])
+                        equivalent_identifiers = [ v['identifier'] for v in equivalent_identifiers ]
+                        biolink_type = normalized.get(curie, {}).get ("type", [])
+                        if 'identifier' in preferred_id:
+                            result['identifiers'][preferred_id['identifier']] = {
+                                "label" : preferred_id.get('label',''),
+                                "equivalent_identifiers" : equivalent_identifiers,
+                                "type" : biolink_type
+                            }
+                        else:
+                            print (f"ERROR: curie:{curie} returned preferred id: {preferred_id}")
+            except json.decoder.JSONDecodeError as e:
+                traceback.print_exc ()
+            except:
+                traceback.print_exc ()
+                raise
         return response
+
 
 def main ():
     """ 
@@ -113,12 +104,17 @@ def main ():
 
     Also, skip words we don't want to consider.
     """
+    config = {
+        'annotator' : "https://api.monarchinitiative.org/api/nlp/annotate/entities?min_length=4&longest_only=false&include_abbreviation=false&include_acronym=false&include_numbers=false&content=",
+        'normalizer' : "https://nodenormalization-sri.renci.org/get_normalized_nodes?curie="
+    }
+    
     input_file = sys.argv[1]
 
     """ Annotate an input file given a set of ontologies. """
     annotator = TOPMedStudyAnnotator (
-        skip=[ 'ever','disease' ],
-        ontologies=[ 'mondo.json' ])
+        config=config,
+        skip=[ 'ever','disease' ])
     response = annotator.annotate (input_file)
 
     """ Write the annotated variables. """
