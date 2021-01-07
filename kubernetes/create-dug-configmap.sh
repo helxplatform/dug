09cd9a5009d43b7b2ad1675633c87caf37ca42bb
PJ Linebaugh <pjl@renci.org>
2021-01-07 18:50:41 -0500

    Removed Dug Helm chart
    The Dug Helm chart is now located in the devops repository.

--- kubernetes/create-dug-configmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NAMESPACE=${NAMESPACE-"default"}
-
-kubectl -n $NAMESPACE delete configmap dug-configmap
-kubectl -n $NAMESPACE create configmap dug-configmap --from-env-file=dug-configmap.properties
