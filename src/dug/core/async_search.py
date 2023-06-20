724809bdf2a112dd9c9db56a9415f4e2172ad182
YaphetKG <kebedey@renci.org>
2023-06-20 15:38:46 -0400

    validation search functions

--- src/dug/core/async_search.py
+++ src/dug/core/async_search.py
@@ -327,7 +327,7 @@ class Search:
                 }
             }
 
-        body = json.dumps({'query': query})
+        body = {'query': query}
         total_items = await self.es.count(body=body, index="variables_index")
         search_results = await self.es.search(
             index="variables_index",
@@ -390,6 +390,194 @@ class Search:
                 new_results = {}
         return new_results
 
+
+    async def search_vars_unscored (self, concept="", query="", size=None, data_type=None, offset=0, fuzziness=1,
+                         prefix_length=3):
+        """
+        In variable search, the concept MUST match one of the identifiers in the list
+        The query can match search_terms (hence, "should") for ranking.
+
+        Results Return
+        The search result is returned in JSON format {collection_id:[elements]}
+
+        Filter
+        If a data_type is passed in, the result will be filtered to only contain
+        the passed-in data type.
+        """
+        query = {
+            'bool': {
+                'should': {
+                    "match": {
+                        "identifiers": concept
+                    }
+                },
+                'should': [
+                    {
+                        "match_phrase": {
+                            "element_name": {
+                                "query": query,
+                                "boost": 10
+                            }
+                        }
+                    },
+                    {
+                        "match_phrase": {
+                            "element_desc": {
+                                "query": query,
+                                "boost": 6
+                            }
+                        }
+                    },
+                    {
+                        "match_phrase": {
+                            "search_terms": {
+                                "query": query,
+                                "boost": 8
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "element_name": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "operator": "and",
+                                "boost": 4
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "search_terms": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "operator": "and",
+                                "boost": 5
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "element_desc": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "operator": "and",
+                                "boost": 3
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "element_desc": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "boost": 2
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "element_name": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "boost": 2
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "search_terms": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length,
+                                "boost": 1
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "optional_terms": {
+                                "query": query,
+                                "fuzziness": fuzziness,
+                                "prefix_length": prefix_length
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+
+        if concept:
+            query['bool']['must'] = {
+                "match": {
+                        "identifiers": concept
+                }
+            }
+
+        body = {'query': query}
+        total_items = await self.es.count(body=body, index="variables_index")
+        search_results = []
+        async for r in async_scan(self.es,
+                                  query=body):
+            search_results.append(r)
+        # Reformat Results
+        new_results = {}
+        if not search_results:
+            # we don't want to error on a search not found
+            new_results.update({'total_items': total_items['count']})
+            return new_results
+
+        for elem in search_results:
+            elem_s = elem['_source']
+            elem_type = elem_s['data_type']
+            if elem_type not in new_results:
+                new_results[elem_type] = {}
+
+            elem_id = elem_s['element_id']
+            coll_id = elem_s['collection_id']
+            elem_info = {
+                "description": elem_s['element_desc'],
+                "e_link": elem_s['element_action'],
+                "id": elem_id,
+                "name": elem_s['element_name']
+            }
+
+            # Case: collection not in dictionary for given data_type
+            if coll_id not in new_results[elem_type]:
+                # initialize document
+                doc = {}
+
+                # add information
+                doc['c_id'] = coll_id
+                doc['c_link'] = elem_s['collection_action']
+                doc['c_name'] = elem_s['collection_name']
+                doc['elements'] = [elem_info]
+
+                # save document
+                new_results[elem_type][coll_id] = doc
+
+            # Case: collection already in dictionary for given element_type; append elem_info.  Assumes no duplicate elements
+            else:
+                new_results[elem_type][coll_id]['elements'].append(elem_info)
+
+        # Flatten dicts to list
+        for i in new_results:
+            new_results[i] = list(new_results[i].values())
+
+        # Return results
+        if bool(data_type):
+            if data_type in new_results:
+                new_results = new_results[data_type]
+            else:
+                new_results = {}
+        return new_results
+
+
     async def search_kg(self, unique_id, query, offset=0, size=None, fuzziness=1, prefix_length=3):
         """
         In knowledge graph search the concept MUST match the unique ID
@@ -425,3 +613,15 @@ class Search:
         search_results.update({'total_items': total_items['count']})
         return search_results
 
+
+if __name__ == '__main__':
+    import asyncio
+    from dug.config import Config
+    cfg = Config(
+        elastic_password="NkFgWqw5lg",
+        elastic_username="elastic",
+        elastic_host="localhost",
+        elastic_port=9200
+    )
+    search_clt = Search(cfg=cfg)
+    asyncio.run(search_clt.search_vars_unscored("MONDO:0021117", "lung neoplasm", size=10_000))
